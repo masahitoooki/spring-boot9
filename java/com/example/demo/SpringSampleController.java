7955e38c748a4556c0d0bfbccbeaf708163d1341
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2021-01-15 10:24:21 +0900

    Initial commit

---
+++ java/com/example/demo/SpringSampleController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SpringSampleController {
+
+	@RequestMapping(value = "/input", method = RequestMethod.GET)
+	public String input(Model model){
+                // 空のフォームオブジェクトをModelに設定
+		model.addAttribute("message", new Message());
+                // 遷移先を返す(この場合はinput.htmlが遷移先となる)
+		return "input";
+	}
+
+	@RequestMapping(value = "/confirm", method = RequestMethod.POST)
+	public String confirm(@ModelAttribute("message") Message form) {
+                // 遷移先を返す(この場合はconfirm.htmlが遷移先となる)
+		return "confirm";
+	}
+
+}
